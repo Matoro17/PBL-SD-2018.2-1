6156a9223dc1b3357c5b67fbcc21110963cdf1a1
Matoro17 <gabrielsilvadeazevedo@gmail.com>
2018-09-13 20:34:55 -0300

    leds part 1

--- project/Nios/synthesis/submodules/Nios_mm_interconnect_0_cmd_xbar_mux_002.sv
+++ project/Nios/synthesis/submodules/Nios_mm_interconnect_0_cmd_xbar_mux_002.sv
@@ -31,8 +31,8 @@
 //   ARBITRATION_SCHEME   "round-robin"
 //   PIPELINE_ARB:        1
 //   PKT_TRANS_LOCK:      58 (arbitration locking enabled)
-//   ST_DATA_W:           92
-//   ST_CHANNEL_W:        3
+//   ST_DATA_W:           94
+//   ST_CHANNEL_W:        5
 // ------------------------------------------
 
 module Nios_mm_interconnect_0_cmd_xbar_mux_002
@@ -41,8 +41,8 @@ module Nios_mm_interconnect_0_cmd_xbar_mux_002
     // Sinks
     // ----------------------
     input                       sink0_valid,
-    input [92-1   : 0]  sink0_data,
-    input [3-1: 0]  sink0_channel,
+    input [94-1   : 0]  sink0_data,
+    input [5-1: 0]  sink0_channel,
     input                       sink0_startofpacket,
     input                       sink0_endofpacket,
     output                      sink0_ready,
@@ -52,8 +52,8 @@ module Nios_mm_interconnect_0_cmd_xbar_mux_002
     // Source
     // ----------------------
     output                      src_valid,
-    output [92-1    : 0] src_data,
-    output [3-1 : 0] src_channel,
+    output [94-1    : 0] src_data,
+    output [5-1 : 0] src_channel,
     output                      src_startofpacket,
     output                      src_endofpacket,
     input                       src_ready,
@@ -64,12 +64,12 @@ module Nios_mm_interconnect_0_cmd_xbar_mux_002
     input clk,
     input reset
 );
-    localparam PAYLOAD_W        = 92 + 3 + 2;
+    localparam PAYLOAD_W        = 94 + 5 + 2;
     localparam NUM_INPUTS       = 1;
     localparam SHARE_COUNTER_W  = 1;
     localparam PIPELINE_ARB     = 1;
-    localparam ST_DATA_W        = 92;
-    localparam ST_CHANNEL_W     = 3;
+    localparam ST_DATA_W        = 94;
+    localparam ST_CHANNEL_W     = 5;
     localparam PKT_TRANS_LOCK   = 58;
 
 	assign	src_valid			=  sink0_valid;
