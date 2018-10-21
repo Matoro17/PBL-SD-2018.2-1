9e43e63c1d33bee35a540d9cb794b2c18b29a533
Gabriel Silva de Azevedo <gabrielsilvadeazevedo@gmail.com>
2018-10-21 13:54:00 -0300

    little fix in lcd driver

--- verilog/lcd_driver.v
+++ verilog/lcd_driver.v
@@ -31,45 +31,37 @@ module lcd_driver (dataa, datab, result, clk, clk_en, start, reset, done, rs, rw
 	reg [1:0] state;
 	
 	// Sempre que haver clock inicia
-	always @ (posedge clk or posedge reset) begin
-		if (reset) begin
-			contador <= 17'd0;
-			rs       <= 1'b0;
-			en       <= 1'b0;
-			db       <= 8'b0;
-			state    <= 1'b0;
-		end else begin
-			if (clk_en) begin
-				case (state)
-					idle:begin
-						done <= 1'b0;
-						if (start) begin
-							state <= working;
-							rs <= dataa[0];
-							db <= datab[7:0];
-							contador <= 17'd0;
-							en <= 1'b1;
-						end else begin
-							state <= idle;
-						end
-					end
-					working:begin
-						done <= 1'b0;
-						if (contador == 17'd100_000) begin
-							state <= finish;
-							en    <= 1'b0;
-						end else begin
-							contador <= contador + 1'd1;
-							state <= working;
-						end
-					end
-					finish:begin
-						done <= 1'b1;
-						result <= 1'b1;
+	always @ (posedge clk) begin
+		if (clk_en) begin
+			case (state)
+				idle:begin
+					done <= 1'b0;
+					if (start) begin
+						en <= 1'b1;
+						state <= working;
+						contador <= 17'd0;
+						rs <= dataa[0];
+						db <= datab[7:0];
+					end else begin
 						state <= idle;
 					end
-				endcase
-			end
+				end
+				working:begin
+					done <= 1'b0;
+					if (contador == 17'd100_000) begin
+						state <= finish;
+						en    <= 1'b0;
+					end else begin
+						state <= working;
+						contador <= contador + 1'd1;
+					end
+				end
+				finish:begin
+					done <= 1'b1;
+					result <= 1'b1;
+					state <= idle;
+				end
+			endcase
 		end
 	end
 	
